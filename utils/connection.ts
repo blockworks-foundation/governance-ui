7e6bfe444b0fcdeffa3f03d6eb17a525bb93d173
Adrian Brzeziński <a.brzezinski94@gmail.com>
2022-12-06 04:21:33 +0100

    bump v mangolana

--- utils/connection.ts
+++ utils/connection.ts
@@ -5,13 +5,11 @@ import type { EndpointInfo } from '../@types/types'
 const ENDPOINTS: EndpointInfo[] = [
   {
     name: 'mainnet',
-    url:
-      process.env.MAINNET_RPC ||
-      'http://realms-realms-c335.mainnet.rpcpool.com/258d3727-bb96-409d-abea-0b1b4c48af29/',
+    url: process.env.MAINNET_RPC || 'http://5.62.126.197:8899',
   },
   {
     name: 'devnet',
-    url: process.env.DEVNET_RPC || 'https://api.dao.devnet.solana.com/',
+    url: process.env.DEVNET_RPC || 'http://5.62.126.197:8899',
   },
   {
     name: 'localnet',
