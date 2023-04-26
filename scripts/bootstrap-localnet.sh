21328c90fbeeed0c9e80607cd4c5001b5b62d0a7
Maximilian Schneider <mail@maximilianschneider.net>
2023-04-26 16:14:01 +0200

    add more instructions

--- scripts/bootstrap-localnet.sh
+++ scripts/bootstrap-localnet.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 
-# use locally compiled solana binaries to access new program dump features
+# clone this branch next to governance-ui: https://github.com/blockworks-foundation/solana/tree/max/dump-poa
+# build with `rustup override set 1.65.0-aarch64-apple-darwin; cargo build`
+# use locally compiled solana-cli binaries to access new dump features
+# in Code/governance-ui run ./scripts/bootstrap-localnet.sh
+# so it finds Code/solana/target/debug/solana
 export PATH="../solana/target/debug:$PATH"
 
 output_path="dump"
@@ -28,11 +32,14 @@ solana account -o $output_path/GovER5Lthms3bLBqWub97yVrMmEogzX7xNjdXpPPCVZw/acco
 
 
 # find token accounts & mints to download through parsing all governances of the realm
+
+# multi-sig on solana-labs' realms instance
 export OUT=$output_path
 export GOV_PROGRAM_ID=GovER5Lthms3bLBqWub97yVrMmEogzX7xNjdXpPPCVZw
 export REALM_ID=Eb69t8zTkNbBViLRykbgBofr8PbwvZoAX4KicVcNNwkj
 yarn ts-node scripts/governance-dump.ts || exit
 
+# mango dao on it's own realms instance
 export GOV_PROGRAM_ID=GqTPL6qRf5aUuqscLh8Rg2HTxPUXfhhAXDptTLhp1t2J
 export VSR_PROGRAM_ID=4Q6WW2ouZ6V3iaNm56MTd5n2tnTm4C5fiH8miFHnAFHo
 export REALM_ID=DPiH3H3c7t47BMxqTxLsuPQpEC6Kne8GA9VXbxpnZxFE
@@ -44,3 +51,4 @@ solana-test-validator --reset \
   --account-dir $output_path/GovER5Lthms3bLBqWub97yVrMmEogzX7xNjdXpPPCVZw/accounts \
   --account-dir $output_path/GqTPL6qRf5aUuqscLh8Rg2HTxPUXfhhAXDptTLhp1t2J/accounts \
   --account-dir $output_path/4Q6WW2ouZ6V3iaNm56MTd5n2tnTm4C5fiH8miFHnAFHo/accounts
+
